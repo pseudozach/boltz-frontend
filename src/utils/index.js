6006a7e8f2b1faf9b2568ea9042d99635b5e7145
pseudozach <zozsoy@gmail.com>
2021-11-17 06:24:39 +0000

    successful sov to LN swap

--- src/utils/index.js
+++ src/utils/index.js
@@ -401,6 +401,326 @@ export const lockTokens = async (swapInfo, swapResponse) => {
   // claimAddress: string,
   // timelock: BigNumberish,
 
+  var erc20tokenabi = [
+    {
+      inputs: [],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Approval',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'from',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'to',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Transfer',
+      type: 'event',
+    },
+    {
+      inputs: [],
+      name: 'DOMAIN_SEPARATOR',
+      outputs: [
+        {
+          internalType: 'bytes32',
+          name: '',
+          type: 'bytes32',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'PERMIT_TYPEHASH',
+      outputs: [
+        {
+          internalType: 'bytes32',
+          name: '',
+          type: 'bytes32',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_owner',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: '_spender',
+          type: 'address',
+        },
+      ],
+      name: 'allowance',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_spender',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: '_value',
+          type: 'uint256',
+        },
+      ],
+      name: 'approve',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_owner',
+          type: 'address',
+        },
+      ],
+      name: 'balanceOf',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'decimals',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'name',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '',
+          type: 'address',
+        },
+      ],
+      name: 'nonces',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'deadline',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint8',
+          name: 'v',
+          type: 'uint8',
+        },
+        {
+          internalType: 'bytes32',
+          name: 'r',
+          type: 'bytes32',
+        },
+        {
+          internalType: 'bytes32',
+          name: 's',
+          type: 'bytes32',
+        },
+      ],
+      name: 'permit',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'symbol',
+      outputs: [
+        {
+          internalType: 'string',
+          name: '',
+          type: 'string',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_to',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: '_value',
+          type: 'uint256',
+        },
+      ],
+      name: 'transfer',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: '_from',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: '_to',
+          type: 'address',
+        },
+        {
+          internalType: 'uint256',
+          name: '_value',
+          type: 'uint256',
+        },
+      ],
+      name: 'transferFrom',
+      outputs: [
+        {
+          internalType: 'bool',
+          name: '',
+          type: 'bool',
+        },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ];
+
+  var erc20tokencontract = new web3.eth.Contract(erc20tokenabi, tokenAddress);
+
   var erc20swapabi = [
     {
       anonymous: false,
@@ -692,6 +1012,8 @@ export const lockTokens = async (swapInfo, swapResponse) => {
     swapResponse.address
   );
   console.log(
+    'erc20 approving ',
+    amount + ' on token ' + tokenAddress,
     'erc20 locking with ',
     preimageHashbuffer,
     amount,
@@ -704,24 +1026,36 @@ export const lockTokens = async (swapInfo, swapResponse) => {
   // , chainId: 33
   let lastBlockGasLimit = web3.eth.getBlock('latest').gasLimit || 0;
   let gasLimit = Math.max(lastBlockGasLimit, 100000);
-  erc20swapcontract.methods
-    .lock(
-      preimageHashbuffer,
-      amount,
-      tokenAddress.toLowerCase(),
-      swapResponse.claimAddress.toLowerCase(),
-      timeout
-    )
-    .call(
-      {
-        from: web3.eth.accounts.currentProvider.selectedAddress,
-        gas: gasLimit,
-      },
-      function(error, transactionHash) {
-        console.log('error: ', error);
-        console.log('transactionHash: ', transactionHash);
-      }
-    );
+
+  erc20tokencontract.methods.approve(swapResponse.address, amount).send(
+    {
+      from: web3.eth.accounts.currentProvider.selectedAddress,
+      gas: gasLimit,
+    },
+    function(error, transactionHash) {
+      console.log('approve error: ', error);
+      console.log('approve transactionHash: ', transactionHash);
+
+      erc20swapcontract.methods
+        .lock(
+          preimageHashbuffer,
+          amount,
+          tokenAddress.toLowerCase(),
+          swapResponse.claimAddress.toLowerCase(),
+          timeout
+        )
+        .send(
+          {
+            from: web3.eth.accounts.currentProvider.selectedAddress,
+            gas: gasLimit,
+          },
+          function(error, transactionHash) {
+            console.log('lock error: ', error);
+            console.log('lock transactionHash: ', transactionHash);
+          }
+        );
+    }
+  );
 };
 
 export const claimFunds = async (swapInfo, swapResponse) => {
